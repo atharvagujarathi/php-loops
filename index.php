87ead68b4e9a7be4e23118733306a5bf2cd721fc
atharvagujarathi <75681555+atharvagujarathi@users.noreply.github.com>
2022-01-15 20:14:47 +0530

    php loops

---
+++ index.php
@@ -0,0 +1,61 @@
+<!-- loops -->
+
+
+
+
+
+<?php 
+
+// 1]. while loop
+
+// $x = 0;
+
+// while( $x < 10 ){
+//     echo " $x Hello world";
+//     $x = $x + 1;
+//     if( $x == 5 ){
+//         echo "condition has to be met $x";
+//         break;
+        
+//     }
+// }
+
+
+
+
+
+// 2]. Do-while loop
+
+// $x = 0;
+// do{
+//     echo " $x Hello world";
+//     $x = $x + 1;
+//     if( $x == 5 ){
+//         echo "yz";
+//         continue;
+//     }
+   
+// }while( $x < 10 );
+
+
+
+
+
+
+// 3]. for loop
+
+// for($x = 0;  $x < 10; $x++){
+//   echo " $x yes ";
+// }
+
+
+
+
+// 4]. foreach loop
+
+$okay = [0, 1, 2, 3, 4, 5, 6];
+
+foreach( $okay as $data)
+echo "$data <br>";
+
+?>
